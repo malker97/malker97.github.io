1787575ca0aacccddfc41c1727155a2d60e5ac65
Zhengmao <malker@qq.com>
2020-06-03 00:24:29 -0700

    这是项目的完整文件

---
+++ README.md
@@ -0,0 +1,1 @@
+![framework](./imgs/framwork.png)
---
+++ searchplayer/searchhero.js
@@ -0,0 +1,67 @@
+// const url = "http://owapi.io/profile/pc/us/臺灣省長蔡英文-4640";
+
+// const url = "http://owapi.io/profile/pc/us/malker-1273";
+var url = "http://owapi.io/profile/pc/us/";
+function getUserID() {
+    var playerid = "";
+    var inputtext = document.getElementById("playerid");
+    playerid = inputtext.value;
+    playerid = playerid.replace(/#/,"-");
+    return playerid;
+}
+
+function myfunction() {
+    var tankrank = "";
+    var damagerank = "";
+    var supportrank = "";
+    var username = "";
+    var userid = getUserID();
+    var newurl = url + userid;
+    // alert(userid);
+    // alert("Its a test for the file");
+    fetch(newurl)
+        .then((res) => res.json())
+        .then((data) => {
+            console.log(data);
+            // if (data.private == false){
+            //     alert("你已经关闭生涯了！");
+            // }
+            tankrank = data.competitive.tank.rank;
+            damagerank = data.competitive.damage.rank;
+            supportrank =  data.competitive.support.rank;
+            if (!tankrank){
+                tankrank = "Unranked";
+            }
+            if (!damagerank){
+                damagerank = "Unranked";
+            }
+            if (!supportrank){
+                supportrank = "Unranked";
+            }
+            username = data.username;
+            document.getElementById("username").innerHTML=username;
+            document.getElementById("headicon").src = data.portrait;
+
+            document.getElementById("supportimg").src="images/resource/support_small.PNG";
+            document.getElementById("tankimg").src="images/resource/tank_small.PNG";
+            document.getElementById("dpsimg").src="images/resource/damage_small.PNG";
+
+            document.getElementById("supportrank").innerHTML="Support: " + supportrank;
+            document.getElementById("tankrank").innerHTML="Tank: " + tankrank;
+            document.getElementById("damagerank").innerHTML="DPS: " + damagerank;
+
+        })
+}
+// alert("这个文件被调用了!");
+// guesstheregion();
+// getIpAddress();
+// myfunction();
+// alert("Its a test for the file");
+// function getIpAddress() {
+//     var newurl = "https://api.ipify.org?format=json";
+//     fetch(newurl)
+//         .then((res) => res.json())
+//         .then((data) => {
+//             alert(data.ip);
+//         })
+// }
